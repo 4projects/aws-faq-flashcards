115089f13d3d51e8bc4e57f416d8b32418bfa854
Stanley Zheng <dev.stanzheng@gmail.com>
2018-01-26 11:05:26 -0500

    replacing serverless framework with SAM

---
+++ server/example/schedule.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+def lambda_sandbox_down_handler(event: dict, context):
+    log.info('running DOWN handler with event: %s and context: %s', event, context)
+    print('pulling dependencies and doing stuff')
+
